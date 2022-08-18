d886cc68fcfb8e521903d683127145d83975ee9e
Prathamesh lendghar <prathameshlendghar78@gmail.com>
2022-08-18 18:28:47 +0530

    Add files via upload

---
+++ assignment-4.c
@@ -0,0 +1,139 @@
+// 1. Write a program to print MySirG 5 times on the screen
+
+// #include<stdio.h>
+// int main(){
+//     int i=1;
+//     while(i<=5){
+//         printf("MysirG\n");
+//         i++;
+//     }
+//     return 0;
+// }
+
+
+// 2. Write a program to print the first 10 natural numbers.
+
+// #include<stdio.h>
+// int main(){
+//     int i=1;
+//     while(i<=10){
+//         printf("%d\n",i);
+//         i++;
+//     }
+//     return 0;
+// }
+
+
+// 3. Write a program to print the first 10 natural numbers in reverse order
+
+// #include<stdio.h>
+// int main(){
+//     int i=10;
+//     while(i>=1){
+//         printf("%d\n",i);
+//         i--;
+//     }
+//     return 0;
+// }
+
+
+// 4. Write a program to print the first 10 odd natural numbers
+
+// #include<stdio.h>
+// int main(){
+//     int i=1,j=1;
+//     while(j<=10){
+//         if(i%2!=0){
+//             printf("%d\n",i);
+//             j++;
+//         }
+//         i++;
+//     }
+//     return 0;
+// }
+
+
+// 5. Write a program to print the first 10 odd natural numbers in reverse order.
+
+// #include<stdio.h>
+// int main(){
+//     int i=20,j=1;
+//     while(j<=10){
+//         if(i%2!=0){
+//             printf("%d\n",i);
+//             j++;
+//         }
+//         i--;
+//     }
+//     return 0;
+// }
+
+
+// 6. Write a program to print the first 10 even natural numbers
+
+// #include<stdio.h>
+// int main(){
+//     int i=1,j=1;
+//     while(j<=10){
+//         if(i%2==0){
+//             printf("%d\n",i);
+//             j++;
+//         }
+//         i++;
+//     }
+//     return 0;
+// }
+
+
+// 7. Write a program to print the first 10 even natural numbers in reverse order
+
+// #include<stdio.h>
+// int main(){
+//     int i=20,j=1;
+//     while(j<=10){
+//         if(i%2==0){
+//             printf("%d\n",i);
+//             j++;
+//         }
+//         i--;
+//     }
+//     return 0;
+// }
+
+// 8. Write a program to print squares of the first 10 natural numbers
+
+// #include<stdio.h>
+// int main(){
+//     int j=1;
+//     while(j<=10){
+//         printf("%d\n",j*j);
+//         j++;    
+//     }
+//     return 0;
+// }
+
+
+// 9. Write a program to print cubes of the first 10 natural numbers
+
+// #include<stdio.h>
+// int main(){
+//     int j=1;
+//     while(j<=10){
+//         printf("%d\n",j*j*j);
+//         j++;    
+//     }
+//     return 0;
+// }
+
+
+// 10. Write a program to print a table of 5.
+
+// #include<stdio.h>
+// int main(){
+//     int j=1;
+//     while(j<=10){
+//         printf("5 X %d = %d\n",j,5*j);
+//         j++;    
+//     }
+//     return 0;
+// }
